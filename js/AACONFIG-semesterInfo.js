6efe249f731823b2d2cdd5c83d410517a4c14f26
David Hovemeyer <david.hovemeyer@gmail.com>
2019-01-17 11:33:06 -0500

    fix first/last days

--- js/AACONFIG-semesterInfo.js
+++ js/AACONFIG-semesterInfo.js
@@ -2,8 +2,8 @@
 var semesterInfo = {
     // The dates for the first and last day of classes (not including finals week).
     // Set the time to 4 AM ... because why not.
-    firstDayOfSemester: new Date("01/23/2018 4:00:00"),
-    lastDayOfSemester:  new Date("05/09/2018 4:00:00"),
+    firstDayOfSemester: new Date("01/23/2019 4:00:00"),
+    lastDayOfSemester:  new Date("05/09/2019 4:00:00"),
 
     // Specify days on which there are no classes held at the college.
     // Each break is specified by a startDate and an endDate.
